17080d4b1916c0c1e771b672a90e38dc6e54582e
mike dunn <mikey.dunn@yahoo.com>
2020-11-16 22:00:43 -0600

    new template

--- templates.js
+++ templates.js
@@ -3,7 +3,13 @@ module.exports.getMessage = (data) => {
     `rawr! notices your bulgy wolgy, *${data.positiveIncrease}* new cases, you're so big :O \n` +
     `rubbies ;) *${data.deathIncrease}* deaths, it doesn't stop gwowing \n` +
     `your icu is at *${data.icuPercentage}%* capacity alweady mmmm~ \n` +
-    `things awe wooking pwetty fuckywucky~ come back tommowo squirms pwetty pwease`
+    `things awe wooking pwetty fuckywucky~ come back tommowo squirms pwetty pwease`,
+
+    `Nyaah! Fwends are still dyiiiing *${data.deathIncrease}* (＾ワ＾)` +
+    `*${data.positiveIncrease}* new cases means DOOOMMMM!!!` +
+    `Hehe ^_^ *${data.icuPercentage}* capacity in your icu, so random!` +
+    `newayz toodles!!!!`
+
   ]
 
   return messageArr[Math.floor(Math.random() * messageArr.length)]
