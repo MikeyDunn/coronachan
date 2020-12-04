49925063fd7f25cfdb0fee6b47bc3a5488aac90f
mike dunn <mikey.dunn@yahoo.com>
2020-12-03 21:45:55 -0600

    simplify

--- scrape.js
+++ scrape.js
@@ -5,63 +5,25 @@ const SLACK_WEBHOOK_PATH = process.env.SLACK_WEBHOOK_PATH
 const https = require('https')
 const templates = require('./templates')
 
-module.exports.handler = async event => {
+module.exports.handler = async () => {
   // request data
   const covidOntarioTodayTrackingPromise = getOntarioCovidTracking(0)
-  const covidOntarioYesterdayTrackingPromise = getOntarioCovidTracking(1)
   const covidTrackingPromise = getTexasCovidTracking()
-  const healthDataPromise = getTexasHealthData()
-  const wolfTexasPromise = askWolf("What is the Population of Texas?")
-  const wolfOntarioPromise = askWolf("What is the Population of Ontario?")
-  const [
-    covidTrackingData,
-    healthData,
-    ontarioTData,
-    ontarioYData,
-    wolfTexasData,
-    wolfOntarioData
-  ] = await Promise.all([
-    covidTrackingPromise,
-    healthDataPromise,
-    covidOntarioTodayTrackingPromise,
-    covidOntarioYesterdayTrackingPromise,
-    wolfTexasPromise,
-    wolfOntarioPromise
-  ])
+  const [covidTrackingData, ontarioTData] = await Promise.all([covidTrackingPromise, covidOntarioTodayTrackingPromise])
 
   // generate messaging
-  const texasMessage = templates.getTexasMessage({ ...covidTrackingData, ...healthData })
-  const ontarioMessage = templates.getOntarioMessage(ontarioTData.result, ontarioYData.result)
+  const raceToTheBottomMessage = templates.getComparisonMessage(covidTrackingData, ontarioTData.result)
 
-   const raceToTheBottomMessage = templates.getComparisonMessage(covidTrackingData, ontarioTData.result, wolfTexasData, wolfOntarioData)
   // post to slack
-   await postToSlack(texasMessage)
-   await postToSlack(ontarioMessage)
-   await postToSlack(raceToTheBottomMessage)	
+  await postToSlack(raceToTheBottomMessage)
 }
 
 function getOntarioCovidTracking(daysToLookBack = 0) {
   return new Promise(resolve => {
     var tzoffset = (new Date()).getTimezoneOffset() * 60000; //offset in milliseconds
-    var localISOTime = (new Date(Date.now() - tzoffset - daysToLookBack*86400000))
+    var localISOTime = (new Date(Date.now() - tzoffset - daysToLookBack * 86400000))
     const urlDate = localISOTime.toISOString().replace(/T.*Z/, "T00:00:00")
     const url = `https://data.ontario.ca/api/3/action/datastore_search?q=${urlDate}&resource_id=ed270bb8-340b-41f9-a7c6-e8ef587e6d11`
-
-    https.get(url, resp => {
-      let data = ''
-
-      resp.on('data', chunk => data += chunk)
-      resp.on('end', () => resolve(JSON.parse(data)))
-    })
-  })
-}
-
-function askWolf(input) {
-  return new Promise(resolve => {
-    const wolfID =  ""
-
-    input = input.replace(/\s/g,'+')
-    const url = `https://api.wolframalpha.com/v2/query?input=${input}&format=moutput&format=plaintext&output=json&appid=${wolfID}`
 
     https.get(url, resp => {
       let data = ''
@@ -86,36 +48,6 @@ function getTexasCovidTracking() {
 
       resp.on('data', chunk => data += chunk)
       resp.on('end', () => resolve(JSON.parse(data)))
-    })
-  })
-}
-
-function getTexasHealthData() {
-  return new Promise(resolve => {
-    const url = 'https://healthdata.gov/dataset/covid-19-estimated-patient-impact-and-hospital-capacity-state/resource/82e733c6-7baa-4c65'
-
-    https.get(url, htmlResp => {
-      let htmlData = ''
-
-      htmlResp.on('data', chunk => htmlData += chunk)
-      htmlResp.on('end', () => {
-        const htmlSplit = htmlData.split('<div class="download"><a href="')[1]
-        const csvUrl = htmlSplit.split('" type="text/csv;')[0]
-
-        https.get(csvUrl, csvResp => {
-          let csvData = ''
-
-          csvResp.on('data', chunk => csvData += chunk)
-          csvResp.on('end', () => {
-            const csvSplit = csvData.split('TX,')
-            const lastSplit = csvSplit[csvSplit.length - 1]
-            const newestTxRecord = lastSplit.split(/\n/)[0]
-            const icuPercentage = newestTxRecord.split(/(?!\B"[^"]*),(?![^"]*"\B)/)[4]
-
-            resolve({ icuPercentage })
-          })
-        })
-      })
     })
   })
 }
--- templates.js
+++ templates.js
@@ -1,56 +1,14 @@
-module.exports.getTexasMessage = (data) => {
-  const messageArr = [
-    `rawr! Texas San !!! notices your bulgy wolgy, *${data.positiveIncrease}* new cases, you're so big :O \n` +
-    `rubbies ;) *${data.deathIncrease}* deaths, it doesn't stop gwowing \n` +
-    `your icu is at *${data.icuPercentage}%* capacity alweady mmmm~ \n` +
-    `things awe wooking pwetty fuckywucky~ come back tommowo squirms pwetty pwease`,
-
-    `Nyaah! Texas San!!! Yur Fwends are still dyiiiing *${data.deathIncrease}* (＾ワ＾) \n` +
-    `*${data.positiveIncrease}* new cases means DOOOMMMM!!! \n` +
-    `Hehe ^_^ *${data.icuPercentage}* capacity in your icu, so random! \n` +
-    `newayz toodles!!!!`
-  ]
-
-  return messageArr[Math.floor(Math.random() * messageArr.length)]
-}
-
-module.exports.getOntarioMessage = (newData, previousData) => {
-  const positiveIncrease = (newData.records[newData.records.length-1][newData.fields[8].id] - previousData.records[0][previousData.fields[8].id])
-  const deathIncrease = (newData.records[newData.records.length-1][newData.fields[7].id] - previousData.records[0][previousData.fields[7].id])
-  const icuIncrease = (newData.records[newData.records.length-1][newData.fields[14].id] - previousData.records[0][previousData.fields[14].id])
-
-  const messageArr = [
-    `I hawent fowgot abouout you Ontawio Sama!!! \n` +
-    `*${positiveIncrease}* new kawaises *${deathIncrease}* deaths and *${icuIncrease}* more in icu is giving me double duty :O \n` +
-    `but i can handle it~~`,
-
-    `Ontawio Sama ^w^  I have you nyew statistics tuou ｡◕ ‿ ◕｡. I'm twacking *${positiveIncrease}* nyew confiwmed kwaises \n` +
-    `*${deathIncrease}* deaths (◠﹏◠✿), *${icuIncrease}* additions to the icu`
-  ]
-
-  return messageArr[Math.floor(Math.random() * messageArr.length)]
-}
-
-module.exports.getComparisonMessage = (texasData, ontarioData, texasPopulationData, ontarioPopulationData) => {
+module.exports.getComparisonMessage = (texasData, ontarioData) => {
   const texasCasesTotal = texasData.positive
-  const ontarioCasesTotal = ontarioData.records[ontarioData.records.length-1][ontarioData.fields[8].id]
-  const texasDeathTotal = texasData.death
-  const ontarioDeathTotal = ontarioData.records[ontarioData.records.length-1][ontarioData.fields[7].id]
-  let texasPopulation = 29000000
-  if (typeof(texasPopulationData.queryresult.pods) !== 'undefined') {
-     texasPopulation =  parseInt(texasPopulationData.queryresult.pods[1].subpods[0].plaintext.split(" ")[0],10) * 1000000
-  }
-  let ontarioPopulation = 14570000
-  if (typeof(ontarioPopulationData.queryresult.pods) !== 'undefined') {
-      ontarioPopulation = parseInt(ontarioPopulationData.queryresult.pods[1].subpods[0].plaintext.split(" ")[0],10) * 1000000 
-  }
-
-  const texasCasePercentage = (texasCasesTotal/texasPopulation) * 100
-  const ontarioCasePercentage = (ontarioCasesTotal/ontarioPopulation) * 100
-  const texasDeathPercentage = (texasDeathTotal/texasPopulation) * 100
-  const ontarioDeathPercentage = (ontarioDeathTotal/ontarioPopulation) * 100
+  const ontarioCasesTotal = ontarioData.records[ontarioData.records.length - 1][ontarioData.fields[8].id]
+  const texasPopulation = 29000000
+  const ontarioPopulation = 14570000
+  const texasCasePercentage = (texasCasesTotal / texasPopulation) * 100
+  const ontarioCasePercentage = (ontarioCasesTotal / ontarioPopulation) * 100
   const messageArr = [
-	  `:texas: *${texasCasePercentage.toFixed(5)}%* :cough1::cough2:\n:maple_leaf: *${ontarioCasePercentage.toFixed(5)}%* :cough1::cough2:\n:texas: *${texasDeathPercentage.toFixed(5)}%* :skull:\n:maple_leaf: *${ontarioDeathPercentage.toFixed(5)}%* :skull:`
+    `Konichiwaaaa Texas!! *${texasCasePercentage.toFixed(2)}%* of your pawpuwation caught the coof so far \n` +
+    `*${ontarioCasePercentage.toFixed(2)}%* of Ontawio too~~ OwO \n` +
+    `Good luck todaay!`
   ]
 
   return messageArr[Math.floor(Math.random() * messageArr.length)]
